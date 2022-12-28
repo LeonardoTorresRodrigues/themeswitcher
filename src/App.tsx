bf1def24ac2fd59e0a8acba67b2e8c86f661e8ac
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-12-28 11:37:08 -0300

    feat: add hook to save the state of the dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
+import usePersistedState from './utils/usePersistedState';
 
 function App() {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
 
   const toggleTheme = () => {
     setTheme(theme.title === 'light' ? dark : light);
